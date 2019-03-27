4bf25cbaa1612ff3e1cab013a813e9c365a372e6
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2019-03-27 11:12:53 +0100

    .

--- a.py
+++ a.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-# pythran export compute_theta(int32[:, :])
-def compute_theta(alignment):
-    n_cols = alignment.shape[0]
-    total = 0
-    for i in range(n_cols):
-        total += np.bincount(alignment[i, :]).sum()
-    return total
+#pythran export compute(int8[:])
+def compute(vector):
+   return np.bincount(vector)
+
 
